52742733fcac7e35ee8112165d0b4aeeeabf551c
Ivern <115958709+pavseh@users.noreply.github.com>
2024-05-24 13:44:42 +0800

    major changes pt.1

--- api_buala.py
+++ api_buala.py
@@ -1,7 +1,12 @@
 from flask import Flask, make_response, jsonify, request
 from flask_mysqldb import MySQL
+from flask_restful import Api, Resource, reqparse
+from flask_jwt import JWT, jwt_required, current_identity
+from werkzeug.security import safe_str_cmp
 
 app = Flask(__name__)
+app.config["SECRET_KEY"] = "super-secret" # Secret Key for JWT
+api = Api(app)
 
 # Main Config of Database
 app.config["MYSQL_HOST"] = "localhost"
@@ -14,6 +19,42 @@ mysql = MySQL(app)
 
 # All Functions
 
+class User(object):
+    def __init__(self, id):
+        self.id = id
+    
+    @classmethod
+    def find_by_username(cls, username):
+        if username == "admin":
+            return cls(id=1)
+
+def authentication(username, password):
+    user = User.find_by_username(username)
+    if user and safe_str_cmp(password.encode("utf-8"), "password".encode("utf-8")):
+        return user
+
+def identity(payload):
+    user_id = payload["identity"]
+    return User.find_by_id(user_id)
+
+
+
+# MAIN
+class Index(Resource):
+    def get(self):
+        return {
+            "message": "Welcome to the Ivern Studios API!",
+            "endpoints": {
+                "View all companies": "/company",
+                "View company by ID": "/company/<int:id>",
+                "Add a new company": "/company",
+                "Update company by ID": "/company/<int:id>",
+                "Delete company by ID": "/company/<int:id>"
+            }
+        }
+
+
+
 # Fetching Data from Database
 def data_fetch(query):
     cur = mysql.connection.cursor()
@@ -23,43 +64,45 @@ def data_fetch(query):
     return data
 
 # Get Company Data Info
-@app.route("/company", methods=["GET"])
-def get_company():
-    data = data_fetch("""SELECT * FROM ivernstudios""")
-    # JSON response return value
-    return make_response(jsonify(data), 200)
+class Company(Resource):
+    def get_company():
+        data = data_fetch("""SELECT * FROM ivernstudios""")
+        # JSON response return value
+        return make_response(jsonify(data), 200)
 
 # Get Company ID Info
-@app.route("/company/<int:id>", methods=["GET"])
-def get_company_by_id(id):
-    data = data_fetch("""SELECT * FROM ivernstudios WHERE id = {}""".format(id))
-    # JSON response return
-    return make_response(jsonify(data), 200)
+class CompanyByID(Resource):
+    def get(self, id):
+        data = data_fetch("""SELECT * FROM ivernstudios WHERE id = {}""".format(id))
+        # JSON response return value
+        return make_response(jsonify(data), 200)
+
 
 # Add Company Info
-@app.route("/company", methods=["POST"])
-def add_company():
+class AddCompany(Resource):
+    def post(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument("name", required=True)
+        parser.add_argument("age", type=int, required=True)
+        parser.add_argument("position", required=True)
+        info = parser.parse_args()
 
-    # Extract Company Data
-    info = request.get_json()
-    name = info.get("name")
-    age = info.get("age")
-    position = info.get("position")
-    
-    # Insert New Company Data
-    cur = mysql.connection.cursor()
-    cur.execute(
-        """INSERT INTO ivernstudios (name, age, position) VALUES (%s, %s, %s)""",
-        (name, age, position),
-    )
-    mysql.connection.commit()
-    cur.close()
-    
-    # Return Value if company added
-    return make_response(
-        jsonify({"message": "company added successfully", "name": name, "age": age, "position": position}),
-        201,
-    )
+        name = info["name"]
+        age = info["age"]
+        position = info["position"]
+        
+        cur = mysql.connection.cursor()
+        cur.execute(
+            """INSERT INTO ivernstudios (name, age, position) VALUES (%s, %s, %s)""",
+            (name, age, position),
+        )
+        mysql.connection.commit()
+        cur.close()
+        
+        return make_response(
+            jsonify({"message": "company added successfully", "name": name, "age": age, "position": position}),
+            201,
+        )
 
 
 # Update Company Info
--- tester_buala.py
+++ tester_buala.py
@@ -6,39 +6,38 @@ class TestAPITester(unittest.TestCase):
     def setUp(self):
         app.config["TESTING"] = True
         self.app = app.test_client()
-
         warnings.simplefilter("ignore", category=DeprecationWarning)
 
 
-    def tester_get_company(self):
+    def test_get_company(self):
         response = self.app.get("/company")
         self.assertEqual(response.status_code, 200)
         self.assertTrue("Ivern Buala" in response.data.decode())
 
 
-    def tester_get_company_id(self):
+    def test_get_company_id(self):
         response = self.app.get("/company/1")
         self.assertEqual(response.status_code, 200)
         self.assertTrue("Ivern Buala" in response.data.decode())
 
 
-    def tester_add_company(self):
+    def test_add_company(self):
         data = {"name": "Ivern Buala", "age": 21, "position": "Founder"}
         response = self.app.post("/company", json=data)
         self.assertEqual(response.status_code, 201)
         self.assertTrue("Ivern Buala" in response.data.decode())
 
-    def tester_update_company(self):
+    def test_update_company(self):
         data = {"name": "Lawrence Apalla", "age": 20, "position": "Co-Founder"}
         response = self.app.put("/company/1", json=data)
         self.assertEqual(response.status_code, 200)
         self.assertTrue("Lawrence Apalla" in response.data.decode())
 
-    def tester_delete_company(self):
+    def test_delete_company(self):
         response = self.app.delete("/company/1")
         self.assertEqual(response.status_code, 200)
         self.assertTrue("deleted successfully" in response.data.decode())
 
 
-if __name__ == "__tester__":
+if __name__ == "__main__":
     unittest.main()
