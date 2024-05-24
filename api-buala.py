a0610ed697bce10d7561aa4ef42faa57662b9250
Ivern <115958709+pavseh@users.noreply.github.com>
2024-05-24 12:13:55 +0800

    Update company info

--- api-buala.py
+++ api-buala.py
@@ -23,23 +23,24 @@ def data_fetch(query):
     cur.close()
     return data
 
-# Get Company Data
+# Get Company Data Info
 @app.route("/company", methods=["GET"])
 def get_company():
     data = data_fetch("""SELECT * FROM company""")
     # JSON response return value
     return make_response(jsonify(data), 200)
 
-# Get Company ID
+# Get Company ID Info
 @app.route("/company/<int:id>", methods=["GET"])
 def get_company_by_id(id):
     data = data_fetch("""SELECT * FROM company WHERE id = {}""".format(id))
     # JSON response return
     return make_response(jsonify(data), 200)
 
-# Add Company
+# Add Company Info
 @app.route("/company", methods=["POST"])
 def add_company():
+    
     # Extract Company Data
     info = request.get_json()
     name = info.get("name")
@@ -59,4 +60,30 @@ def add_company():
     return make_response(
         jsonify({"message": "company added successfully", "name": name, "age": age, "position": position}),
         201,
+    )
+
+
+# Update Company Info
+@app.route("/company/<int:id>", methods=["PUT"])
+def update_company(id):
+
+    # Extract Updated Company Data
+    info = request.get_json()
+    name = info.get("name")
+    age = info.get("age")
+    position = info.get("position")
+    
+    # Update Company Data
+    cur = mysql.connection.cursor()
+    cur.execute(
+        """UPDATE company SET name = %s, age = %s, position = %s WHERE id = %s""",
+        (name, age, position, id),
+    )
+    mysql.connection.commit()
+    cur.close()
+    
+    # Return Value if company updated
+    return make_response(
+        jsonify({"message": "company updated successfully", "id": id, "name": name, "age": age, "position": position}),
+        200,
     )
