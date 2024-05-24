9cc1eaf0775d607581883018be0d3ff3bb57fa4b
Ivern <115958709+pavseh@users.noreply.github.com>
2024-05-24 12:37:20 +0800

    tester add, update, delete

--- tester-buala.py
+++ tester-buala.py
@@ -11,13 +11,34 @@ class APITester(unittest.TestCase):
 
 
     def tester_get_company(self):
-        
         response = self.app.get("/company")
         self.assertEqual(response.status_code, 200)
         self.assertTrue("Ivern Buala" in response.data.decode())
 
-    def tester_get_company_id(self):
 
+    def tester_get_company_id(self):
         response = self.app.get("/company/1")
         self.assertEqual(response.status_code, 200)
         self.assertTrue("Ivern Buala" in response.data.decode())
+
+
+    def tester_add_company(self):
+        data = {"name": "Ivern Buala", "age": 21, "position": "Founder"}
+        response = self.app.post("/company", json=data)
+        self.assertEqual(response.status_code, 201)
+        self.assertTrue("Ivern Buala" in response.data.decode())
+
+    def tester_update_company(self):
+        data = {"name": "Lawrence Apalla", "age": 20, "position": "Co-Founder"}
+        response = self.app.put("/company/1", json=data)
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue("Lawrence Apalla" in response.data.decode())
+
+    def tester_delete_company(self):
+        response = self.app.delete("/company/1")
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue("deleted successfully" in response.data.decode())
+
+
+if __name__ == "__tester__":
+    unittest.main()
