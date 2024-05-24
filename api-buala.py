bcbf335ec497521f9f6fb86b3d383a86db704069
Ivern <115958709+pavseh@users.noreply.github.com>
2024-05-24 12:11:25 +0800

    Add company Info

--- api-buala.py
+++ api-buala.py
@@ -36,3 +36,27 @@ def get_company_by_id(id):
     data = data_fetch("""SELECT * FROM company WHERE id = {}""".format(id))
     # JSON response return
     return make_response(jsonify(data), 200)
+
+# Add Company
+@app.route("/company", methods=["POST"])
+def add_company():
+    # Extract Company Data
+    info = request.get_json()
+    name = info.get("name")
+    age = info.get("age")
+    position = info.get("position")
+    
+    # Insert New Company Data
+    cur = mysql.connection.cursor()
+    cur.execute(
+        """INSERT INTO company (name, age, position) VALUES (%s, %s, %s)""",
+        (name, age, position),
+    )
+    mysql.connection.commit()
+    cur.close()
+    
+    # Return Value if company added
+    return make_response(
+        jsonify({"message": "company added successfully", "name": name, "age": age, "position": position}),
+        201,
+    )
