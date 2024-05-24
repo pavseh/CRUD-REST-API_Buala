378bc256daf1cdf7f06b1fbcfb34d7404d7c7aa1
Ivern <115958709+pavseh@users.noreply.github.com>
2024-05-24 12:33:24 +0800

    tester get company & its id

--- tester-buala.py
+++ tester-buala.py
@@ -1,3 +1,23 @@
 import unittest
 import warnings
 from api import app
+
+class APITester(unittest.TestCase):
+    def setup(self):
+        app.config["TESTING"] = True
+        self.app = app.test_client()
+
+        warnings.simplefilter("ignore", category=DeprecationWarning)
+
+
+    def tester_get_company(self):
+        
+        response = self.app.get("/company")
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue("Ivern Buala" in response.data.decode())
+
+    def tester_get_company_id(self):
+
+        response = self.app.get("/company/1")
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue("Ivern Buala" in response.data.decode())
